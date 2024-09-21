de46922e4ef30b1feab8fec35abe1fb3141d7fa6
clara seijo <clasei@proton.me>
2024-09-21 10:38:42 +0200

    iteration 1 done; Counter logic implemented in App; App mananges the state/changes and passes them as props to Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./App.css";
 import Counter from "./components/Counter";
 import ExponentTwo from "./components/ExponentTwo";
@@ -8,11 +10,22 @@ import ExponentSix from "./components/ExponentSix";
 
 
 function App () {
+
+  // count state moved to App to pass it to other child components as props
+  // now count state is managed from App and passed to Counter; same working logic
+  const [count, setCount] = useState(0);
+
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
+
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
-  
-      <Counter/>
+      
+      {/* passing count, decrement, increment as props to counter to update changes */}
+      <Counter count={count} onDecrement={decrement} onIncrement={increment}/>
 
       <br />
       <h2><em>Exponents</em></h2>
